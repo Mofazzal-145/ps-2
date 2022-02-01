0d1902cd5dde00f787db783f67e358506ef7a22f
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-01 23:33:08 +0600

    first file

---
+++ first.js
@@ -0,0 +1,12 @@
+
+var sentence = "I am hardworking. I am determined. I will be a web developer.";
+var count = 0;
+for(var i = 0; i < sentence.length; i++){
+  var letter = sentence[i];
+  if(letter == 'a'){
+    count++;
+  }
+}
+console.log('Total a count is',count);
+
+
